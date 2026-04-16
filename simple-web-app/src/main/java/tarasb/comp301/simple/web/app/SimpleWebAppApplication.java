a7990653e24cefdc33f648989f4844fb938b48c1
tarasb-source” <taras.brytskiy@gmail.com>
2026-04-16 14:58:32 -0400

    Test

---
+++ simple-web-app/src/main/java/tarasb/comp301/simple/web/app/SimpleWebAppApplication.java
@@ -0,0 +1,12 @@
+package tarasb.comp301.simple.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleWebAppApplication.class, args);
+  }
+}
---
+++ simple-web-app/src/test/java/tarasb/comp301/simple/web/app/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tarasb.comp301.simple.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+}
