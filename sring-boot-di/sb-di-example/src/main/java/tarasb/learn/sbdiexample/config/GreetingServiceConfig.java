0a8d07b37d74df250e90c03d83aa9b021842cd36
tarasb-source” <taras.brytskiy@gmail.com>
2026-04-18 15:26:31 -0400

    Added factory pattern to the java configuration

--- sring-boot-di/sb-di-example/src/main/java/tarasb/learn/sbdiexample/config/GreetingServiceConfig.java
+++ sring-boot-di/sb-di-example/src/main/java/tarasb/learn/sbdiexample/config/GreetingServiceConfig.java
@@ -4,12 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import tarasb.learn.pets.PetService;
+import tarasb.learn.pets.PetServiceFactory;
 import tarasb.learn.sbdiexample.repositories.EnglishGreetingRepository;
 import tarasb.learn.sbdiexample.repositories.EnglishGreetingRepositoryImpl;
 import tarasb.learn.sbdiexample.services.*;
 
 @Configuration
 public class GreetingServiceConfig {
+
+  @Bean
+  PetServiceFactory petServiceFactory() {
+    return new PetServiceFactory();
+  }
+
+  @Profile({"dog", "default"})
+  @Bean
+  PetService dogPetService(PetServiceFactory petServiceFactory) {
+    return petServiceFactory.getService("dog");
+  }
+
+  @Profile("cat")
+  @Bean
+  PetService catPetService(PetServiceFactory petServiceFactory) {
+    return petServiceFactory.getService("cat");
+  }
 
   @Profile({"ES", "default"})
   @Bean("i18nService")
