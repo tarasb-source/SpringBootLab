20a286988f4c7063427cc8674ccfd18552de70aa
tarasb-source” <taras.brytskiy@gmail.com>
2026-04-18 15:59:54 -0400

    Organized the repository

---
+++ chuck-norris-jokes-app/chuck-norris-jokes-app/src/test/java/tarasb/learn/chucknorrisjokesapp/ChuckNorrisJokesAppApplicationTests.java
@@ -0,0 +1,11 @@
+package tarasb.learn.chucknorrisjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesAppApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
